3348c75d9142b69c48be5b444868be0a12111bc4
zeinab_mtawaj <zeinab_mtawaj@PC>
2023-02-11 06:31:11 +0300

    detail

--- Customer/Controllers/ProductController.cs
+++ Customer/Controllers/ProductController.cs
@@ -41,14 +41,29 @@ namespace Customer.Controllers
                      lambdas_spec.Add(x => x.Specification);*/
             var i = 0;
             Specification spec;
+            var lambdas_spec = new List<Expression<Func<Specification, object>>>();
+            lambdas.Add(x => x.ProductSpecificationValues);
+            string name = "";
             if (product_detail.ProductSpecificationValues != null)
             {
                 foreach (var item in product_detail.ProductSpecificationValues)
                 {
+                    name = "";
                     spec = _uow.SpecificationRepo.Find(x => x.Id == item.SpecificationId);
+                    var Specs_for_same_product = _uow.SpecificationRepo.FindAll(x =>(
+                    ( x.Name == spec.Name) && (x.ProductSpecificationValues !=null) &&(x.ProductSpecificationValues.Any(z=>z.ProductId==product_detail.Id))
+                    ),null, null, lambdas_spec);
+
+                    
+                    foreach (var spec_same in Specs_for_same_product)
+                    {
+                        name +=spec_same.Name+"~";
+
+                    }
+                    name = name.Remove(name.Length - 1, 1);
                     product_detail.ProductSpecificationValues.ElementAt(i).Specification = new Specification
                     {
-                        Name = spec.Name
+                        Name =name
                     };
                     i++;
 
